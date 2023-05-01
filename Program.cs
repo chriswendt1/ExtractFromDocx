6cde8b4755c35cca2a5ea009229d91678420db64
Chris Wendt <chris.wendt@outlook.com>
2023-05-01 14:17:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Extract text from parallel text documents in TSV table format");
+
+if (args.Length > 0)
+{
+    string rootPath = args[0];
+    const string outFileName = "collection.tsv";
+    string outFile = rootPath + Path.DirectorySeparatorChar + outFileName;
+    File.Delete(outFile);
+    TraverseDirectory(rootPath, outFile, ProcessFile);
+    return 0;
+}
+else
+    Console.WriteLine("ERROR: Please provide a folder path as argument.");
+return 1;
+
+void TraverseDirectory(string path, string outFile, Action<string, string> action)
+{
+    foreach (string file in Directory.GetFiles(path))
+    {
+        if (file.ToUpperInvariant().Contains("_EN.") && (Path.GetExtension(file).ToLowerInvariant() == ".txt"))
+        {
+            action(file, outFile);
+            continue;
+        }
+    }
+
+    foreach (string directory in Directory.GetDirectories(path))
+    {
+        TraverseDirectory(directory, outFile, action);
+    }
+}
+
+void ProcessFile(string txtFileName, string outFileName)
+{
+    Debug.WriteLine(txtFileName);
+    Console.Write(".");
+    List<string> textsSource = new();
+    
+    textsSource = File.ReadAllLines(txtFileName, System.Text.Encoding.UTF8).ToList();
+
+
+    string targetFileName = txtFileName.Replace("_EN.", "_ES.");
+    if (!File.Exists(targetFileName))
+    {
+        Console.WriteLine($"ERROR: Target file {targetFileName} not found.");
+        return;
+    }
+    List<string> textsTarget = new();
+    textsTarget = File.ReadAllLines(targetFileName, System.Text.Encoding.UTF8).ToList();
+
+    StreamWriter outFile = File.AppendText(outFileName);
+
+    List<TextPair> pairs = TextPairs.Align(textsSource, textsTarget);
+    foreach (TextPair pair in pairs)
+    {
+        outFile.WriteLine(pair.sourceText + "\t" + pair.targetText);
+    }
+    outFile.Close();
+}
---
+++ TextPair.cs
@@ -0,0 +1,36 @@
+﻿struct TextPair
+{
+    public string? sourceText;
+    public string? targetText;
+}
+
+static class TextPairs
+{
+    public static List<TextPair> Align(List<string> sourceTexts, List<string> targetTexts)
+    {
+        List<TextPair> pairs = new();
+
+        for (int i = 0; i < Math.Max(sourceTexts.Count, targetTexts.Count); i++)
+        {
+            TextPair pair = new();
+            if (i >= sourceTexts.Count)
+            {
+                pair.sourceText = null;
+                pair.targetText = targetTexts[i].Trim();
+            }
+            else if (i >= targetTexts.Count)
+            {
+                pair.sourceText = sourceTexts[i].Trim();
+                pair.targetText = null;
+            }
+            else
+            {
+                pair.sourceText = sourceTexts[i].Trim();
+                pair.targetText = targetTexts[i].Trim();
+            }
+            if (pair.sourceText is null && pair.targetText is null) continue;
+            pairs.Add(pair);
+        }
+        return pairs;
+    }
+}
